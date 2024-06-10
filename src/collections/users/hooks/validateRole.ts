f74989612ba51612392b34fed2cfabd7e4470432
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-06-10 22:24:33 +0300

    d

--- src/collections/users/hooks/validateRole.ts
+++ src/collections/users/hooks/validateRole.ts
@@ -4,6 +4,7 @@ import APIError from "payload/dist/errors/APIError";
 export const validateRole: CollectionBeforeOperationHook = async ({ args, operation, req }) => {
   try {
     if (operation == "create" || operation == "update") {
+      return true;
       switch (req.user.role) {
         case "super_admin":
           break;
