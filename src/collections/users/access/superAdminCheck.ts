6d58096d9ed6eb448d502098ac34248bfbd46f21
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-06-11 00:29:55 +0300

    Update superAdminCheck.ts

--- src/collections/users/access/superAdminCheck.ts
+++ src/collections/users/access/superAdminCheck.ts
@@ -2,6 +2,7 @@ import type { Access } from "payload/types";
 import { checkRole } from "../../hooks/checkRole";
 
 const isSuperAdmin: Access = ({ req: { user } }) => {
+  console.log(user);
   if (user) {
     try {
       return checkRole(["super_admin"], user);
