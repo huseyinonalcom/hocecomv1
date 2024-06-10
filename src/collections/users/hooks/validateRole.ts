69ed2c6cef418aa729b37a911919549c261ef7e1
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-06-10 23:01:18 +0300

    Update validateRole.ts

--- src/collections/users/hooks/validateRole.ts
+++ src/collections/users/hooks/validateRole.ts
@@ -11,30 +11,28 @@ export const validateRole: CollectionBeforeChangeHook = async ({ req }) => {
       page: 1,
       limit: 1,
     });
-    console.log(users);
-    console.log(users.docs);
-    console.log(users.totalDocs);
-    console.log(req.user);
-    switch (req.user.role) {
-      case "super_admin":
-        break;
-      case "website":
-        if (req.body.role != "customer") {
-          throw new APIError("You do not have permission to create a user with this role.", 403);
-        }
-        break;
-      case "employee":
-        if (req.body.role != "customer") {
-          throw new APIError("You do not have permission to create a user with this role.", 403);
-        }
-        break;
-      case "admin":
-        if (req.body.role != "customer" && req.body.role != "employee") {
-          throw new APIError("You do not have permission to create a user with this role.", 403);
-        }
-        break;
-      default:
-        throw new APIError("No user detected.", 403);
+    if (users.totalDocs > 0) {
+      switch (req.user.role) {
+        case "super_admin":
+          break;
+        case "website":
+          if (req.body.role != "customer") {
+            throw new APIError("You do not have permission to create a user with this role.", 403);
+          }
+          break;
+        case "employee":
+          if (req.body.role != "customer") {
+            throw new APIError("You do not have permission to create a user with this role.", 403);
+          }
+          break;
+        case "admin":
+          if (req.body.role != "customer" && req.body.role != "employee") {
+            throw new APIError("You do not have permission to create a user with this role.", 403);
+          }
+          break;
+        default:
+          throw new APIError("No user detected.", 403);
+      }
     }
   } catch (e) {
     console.log(e);
