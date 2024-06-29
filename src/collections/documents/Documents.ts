3f0f5d4603d346879dd8d51da6d10d3f0c3e2178
fjhus16 <65453275+huseyinonalcom@users.noreply.github.com>
2024-06-29 19:12:31 +0200

    Update Documents.ts

--- src/collections/documents/Documents.ts
+++ src/collections/documents/Documents.ts
@@ -1,7 +1,6 @@
 import { CollectionConfig } from "payload/types";
 import isSuperAdmin from "../users/access/superAdminCheck";
 import { setCompanyHook } from "../hooks/setCompany";
-import { fieldSelectionHook } from "../hooks/field-selection-hook";
 import payload from "payload";
 
 const Documents: CollectionConfig = {
