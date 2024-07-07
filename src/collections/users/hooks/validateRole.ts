1fa7a234dc32b23fcd9f6ae4407024c1eb6588bc
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-07 16:13:32 +0300

    Update validateRole.ts

--- src/collections/users/hooks/validateRole.ts
+++ src/collections/users/hooks/validateRole.ts
@@ -12,54 +12,37 @@ export const validateRole: CollectionBeforeChangeHook = async ({ req }) => {
       limit: 1,
     });
     if (users.totalDocs > 0) {
-      switch (req.user.role) {
-        case "super_admin":
-          break;
-        case "website":
-          if (req.body.role && req.body.role != "customer") {
-            throw new APIError(
-              "You do not have permission to create a user with this role.",
-              403
-            );
-          }
-          break;
-        case "employee":
-          if (req.body.role && req.body.role != "customer") {
-            throw new APIError(
-              "You do not have permission to create a user with this role.",
-              403
-            );
-          }
-          break;
-        case "admin":
-          if (
-            req.body.role &&
-            req.body.role != "customer" &&
-            req.body.role != "employee"
-          ) {
-            throw new APIError(
-              "You do not have permission to create a user with this role.",
-              403
-            );
-          }
-          break;
-        case "customer":
-          if (req.body.role && req.body.role != "customer") {
-            throw new APIError(
-              "You do not have permission to create a user with this role.",
-              403
-            );
-          }
-          break;
-        default:
-          throw new APIError("No user detected.", 403);
+      if (req.body.role && req.body.role != "customer") {
+        switch (req.user.role) {
+          case "super_admin":
+            break;
+          case "website":
+            if (req.body.role && req.body.role != "customer") {
+              throw new APIError("You do not have permission to create a user with this role.", 403);
+            }
+            break;
+          case "employee":
+            if (req.body.role && req.body.role != "customer") {
+              throw new APIError("You do not have permission to create a user with this role.", 403);
+            }
+            break;
+          case "admin":
+            if (req.body.role && req.body.role != "customer" && req.body.role != "employee") {
+              throw new APIError("You do not have permission to create a user with this role.", 403);
+            }
+            break;
+          case "customer":
+            if (req.body.role && req.body.role != "customer") {
+              throw new APIError("You do not have permission to create a user with this role.", 403);
+            }
+            break;
+          default:
+            throw new APIError("No user detected.", 403);
+        }
       }
     }
   } catch (e) {
     console.log(e);
-    throw new APIError(
-      "You do not have permission to create a user with this role.",
-      403
-    );
+    throw new APIError("You do not have permission to create a user with this role.", 403);
   }
 };
