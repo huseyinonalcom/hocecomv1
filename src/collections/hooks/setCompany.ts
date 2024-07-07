a4f2ebf27517d37c06973013f3b7640a33f8aaa2
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-07 15:50:36 +0300

    Update setCompany.ts

--- src/collections/hooks/setCompany.ts
+++ src/collections/hooks/setCompany.ts
@@ -2,6 +2,9 @@ import { CollectionBeforeChangeHook } from "payload/types";
 import isSuperAdmin from "../users/access/superAdminCheck";
 
 export const setCompanyHook: CollectionBeforeChangeHook = async ({ data, req }) => {
+  console.log("setCompanyHook");
+  console.log(data);
+  console.log(req.user);
   try {
     if (req.user) {
       if (!isSuperAdmin({ req })) {
