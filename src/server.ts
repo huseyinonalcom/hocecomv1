2fea51f6717867da3ff447e07c4fd4a9129769d1
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-07-08 00:04:45 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ const start = async () => {
   app.listen(3421);
   console.log("Server started on http://localhost:3421");
 
-  cron.schedule("*/1 * * * *", async () => {
+  cron.schedule("*/2 * * * *", async () => {
     try {
       console.log("Cron job started");
       await createDocumentsFromBolOrders();
