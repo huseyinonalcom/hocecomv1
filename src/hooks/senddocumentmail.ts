206cecb6629f21707d882af3774cdf14aaaab29c
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-19 21:26:04 +0300

    Update senddocumentmail.ts

--- src/hooks/senddocumentmail.ts
+++ src/hooks/senddocumentmail.ts
@@ -3,18 +3,23 @@ import { sendMail } from "../utils/sendmail";
 
 const sendDocumentMail = async ({ operation, doc }) => {
   try {
+    let pdf = null;
+    try {
+      pdf = await generateInvoice({
+        document: doc,
+        establishment: doc.establishment,
+      });
+    } catch (error) {
+      pdf = null;
+      console.log(error);
+    }
     // doc doesn't have the populated data yet
     // so we need to populate it
     await sendMail({
       recipient: "huseyin-_-onal@hotmail.com",
       subject: `Bestelling ${doc.prefix ?? ""}${doc.number}`,
       company: doc.company,
-      attachments: [
-        await generateInvoice({
-          document: doc,
-          establishment: doc.establishment,
-        }),
-      ],
+      attachments: [pdf],
       html: `<p>Beste ${
         doc.customer.firstName + " " + doc.customer.lastName
       },</p><p>In bijlage vindt u het factuur voor uw laatste bestelling bij ons.</p><p>Met vriendelijke groeten.</p><p>${doc.company.name}</p>`,
