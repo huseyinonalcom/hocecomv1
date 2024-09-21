a1ed4f19216a1b734b0f4632fa12d1d5a6d4b303
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-21 23:52:52 +0300

    Update emailPrefix.ts

--- src/collections/users/hooks/emailPrefix.ts
+++ src/collections/users/hooks/emailPrefix.ts
@@ -2,6 +2,14 @@ import { CollectionBeforeOperationHook } from "payload/types";
 import isSuperAdmin from "../access/superAdminCheck";
 import APIError from "payload/dist/errors/APIError";
 
+// company query param denotes which company the acoount belongs to
+// we add this to the email as a tag so we can differentiate between accounts for different companies (tenants) with the same email
+// no company query means the user might be trying to login from the admin panel
+// probably won't succeed, but even if they succeed, they won't be able to do anything
+// they can't do with the API anyways
+// if it isn't the case, not having a company query param will fail validation
+// we can't block requests without a company query param because the admin panel uses the same API
+
 export const emailPrefix: CollectionBeforeOperationHook = async ({ args, operation, req }) => {
   try {
     if (isSuperAdmin({ req })) {
@@ -9,22 +17,17 @@ export const emailPrefix: CollectionBeforeOperationHook = async ({ args, operati
     } else if (operation == "login" || operation == "forgotPassword") {
       if (req.query.company) {
         const company = req.query.company;
-        // company query param denotes which company the acoount belongs to
-        // we add this to the email as a tag so we can differentiate between accounts for different companies (tenants) with the same email
+
         if (req.body?.email) {
-          console.log('rest api request');
+          console.log("rest api request");
           req.body.email = `${req.body.email.split("@")[0]}+${company}@${req.body.email.split("@")[1]}`;
         } else if (req.body?.variables?.email) {
           // Handling GraphQL requests (email in req.body.variables)
-          console.log('graphql request');
-          req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${company}+${company}@${req.body.variables.email.split("@")[1]}`;
+          console.log("graphql request");
+          req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${company}@${req.body.variables.email.split("@")[1]}`;
         }
         console.log(req.body);
       }
-      // no company query means the user might be trying to login from the admin panel
-      // probably won't succeed, but even if they succeed, they won't be able to do anything
-      // they can't do with the API anyways
-      // if it isn't the case, not having a company query param will fail validation
     }
   } catch (e) {
     console.log(e);
