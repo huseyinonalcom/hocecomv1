a5e01299f999975a40c181803f15df2d0363c4ed
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-21 23:22:26 +0300

    Update emailPrefix.ts

--- src/collections/users/hooks/emailPrefix.ts
+++ src/collections/users/hooks/emailPrefix.ts
@@ -20,9 +20,18 @@ export const emailPrefix: CollectionBeforeOperationHook = async ({ args, operati
   } catch (e) {
     try {
       // graphql version
-      if (req.query.company) {
-        req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${req.query.company}@${req.body.variables.email.split("@")[1]}`;
+      if (isSuperAdmin({ req })) {
+        return args;
+      } else if (operation == "login" || operation == "forgotPassword") {
+        if (req.query.company) {
+          req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${req.query.company}@${req.body.variables.email.split("@")[1]}`;
+        }
+        // no company query means the user might be trying to login from the admin panel
+        // probably won't succeed, but even if they succeed, they won't be able to do anything
+        // they can't do with the API anyways
+        // if it isn't the case, not having a company query param will fail validation
       }
+    
       console.log(req.body);
     } catch (e) {
       throw new APIError("No company could be determined for this user.", 403);
