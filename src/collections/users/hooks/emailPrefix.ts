42c78ddd142379dfffecd077f94d5686a14c140b
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-21 23:26:56 +0300

    Update emailPrefix.ts

--- src/collections/users/hooks/emailPrefix.ts
+++ src/collections/users/hooks/emailPrefix.ts
@@ -8,9 +8,17 @@ export const emailPrefix: CollectionBeforeOperationHook = async ({ args, operati
       return args;
     } else if (operation == "login" || operation == "forgotPassword") {
       if (req.query.company) {
+        const company = req.query.company;
         // company query param denotes which company the acoount belongs to
-        // we add this to the email as a tag so we can differentiate between accounts with the same email
+        // we add this to the email as a tag so we can differentiate between accounts for different companies (tenants) with the same email
         req.body.email = `${req.body.email.split("@")[0]}+${req.query.company}@${req.body.email.split("@")[1]}`;
+
+        if (req.body?.email) {
+          req.body.email = `${req.body.email.split("@")[0]}+${company}@${req.body.email.split("@")[1]}`;
+        } else if (req.body?.variables?.email) {
+          // Handling GraphQL requests (email in req.body.variables)
+          req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${company}@${req.body.variables.email.split("@")[1]}`;
+        }
       }
       // no company query means the user might be trying to login from the admin panel
       // probably won't succeed, but even if they succeed, they won't be able to do anything
@@ -18,20 +26,9 @@ export const emailPrefix: CollectionBeforeOperationHook = async ({ args, operati
       // if it isn't the case, not having a company query param will fail validation
     }
   } catch (e) {
-    try {
-      // graphql version
-      if (isSuperAdmin({ req })) {
-        return args;
-      } else if (operation == "login" || operation == "forgotPassword") {
-        if (req.query.company) {
-          req.body.variables.email = `${req.body.variables.email.split("@")[0]}+${req.query.company}@${req.body.variables.email.split("@")[1]}`;
-        }
-      }
-
-      console.log(req.body);
-    } catch (e) {
-      throw new APIError("No company could be determined for this user.", 403);
-    }
+    console.log(req.body);
+    console.log(e);
+    throw new APIError("No company could be determined for this user.", 403);
   }
 
   return args;
