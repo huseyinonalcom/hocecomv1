82a4ea5a72614b3b8e71d628ba8333c88e907459
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-09-22 18:09:41 +0300

    Update hide-deleted.ts

--- src/collections/hooks/hide-deleted.ts
+++ src/collections/hooks/hide-deleted.ts
@@ -2,8 +2,10 @@ import { CollectionBeforeReadHook } from "payload/types";
 import { checkRole } from "./checkRole";
 
 export const hideDeletedHook: CollectionBeforeReadHook = async ({ query, req }) => {
+  console.log("hideDeletedHook");
+  console.log(req.user);
+  console.log(req.query);
   if (checkRole(["website", "customer"], req.user)) {
-    console.log(req.query);
     //   args.where = {
     //     ...args.where,
     //     isDeleted: { equals: false },
